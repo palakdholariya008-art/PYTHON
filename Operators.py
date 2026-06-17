7472b5060a370a4bcd45f7e3e3d8e7e528995ab5
palakdholariya008-art <palakdholariya008@gmail.com>
2026-06-17 08:01:21 +0530

    Day 2

---
+++ Operators.py
@@ -0,0 +1,174 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+x=10
+
+y=x+20
+
+#arithmetic operator
+
+num1=10
+num2=5
+
+num1
+10
+num2
+5
+
+#+ -> addition
+num1 + num2
+15
+
+# - -> subtraction
+
+num1 -num2
+5
+
+# * -> multiplication
+
+num1* num2
+50
+
+#/ -> division
+num1/num2
+2.0
+
+# // -> floor division
+num1//num2
+2
+
+10/3
+3.3333333333333335
+10//3
+3
+
+# % -> Modulus
+10%3
+1
+
+num1%num2
+0
+
+# ** => exponent
+
+5**2
+25
+3**4
+81
+
+#Assignment operators
+
+val = 100
+result=num1+num2 # here = (equal) and + (arithmetic oper)
+
+result
+15
+
+x=10
+x
+10
+
+x+=1
+x
+11
+# x=x+1
+
+x-=6
+x
+5
+x*=10
+x
+50
+
+x/=10
+x
+5.0
+
+
+##comparison operators
+
+# == , != , > , < , >= , <=
+
+ # ==
+ 
+num1=100
+num2=90
+num3=80
+
+num2==num3
+False
+
+num1==num2
+False
+
+'python' == "Python"
+False
+
+
+# !=
+
+num1 != num2
+True
+
+num2 != num3
+True
+
+# > -greater than
+
+num1> num2
+True
+
+num3>num2
+False
+
+# < -less than
+
+num1< num2
+False
+
+num2<num3
+False
+
+num3<num2
+True
+
+# >= -
+
+num1 >= num2
+True
+
+num2 >= num3
+True
+
+# <=
+num1 <= num2
+False
+num2 <= num3
+False
+
+
+# Logical operators => and,or not
+
+#and
+
+True and True
+True
+
+>>> True and False
+False
+>>> 
+>>> False and True
+False
+>>> 
+>>> 
+>>> #or
+>>> 
+>>> True or True
+True
+>>> 
+>>> True or False
+True
+>>> 
+>>> False or True
+True
+>>>
+
